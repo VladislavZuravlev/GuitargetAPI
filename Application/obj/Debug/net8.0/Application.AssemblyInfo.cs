eb6c4974745b2913db1609954ead380e627202de
zuravlevvladislav19@gmail.com <zuravlevvladislav19@gmail.com>
2024-04-29 14:40:56 +0300

    drop bin and obj

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9b87cfea3fd07f214b7c07ea58ebadc77d380db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e85bab8dcd3b7a6330beb521639844f22945484f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
